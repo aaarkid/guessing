685ae7153775ea2b079402140453aca089cb42e7
aaarkid <kaleciarkid@gmail.com>
2022-07-10 13:15:09 +0200

    better handle errors

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,21 @@ fn main() {
             .expect("Failed to read line");
 
 
-        let guess: u8 = guess.trim().parse().expect("Please type a number!");
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    }
 
         println!("You guessed: {guess}");
 
             match guess.cmp(&secret_number) {
                 Ordering::Less => println!("Too small!"),
                 Ordering::Greater => println!("Too big!"),
-                Ordering::Equal => println!("You win!"),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
             }
     }
 }
